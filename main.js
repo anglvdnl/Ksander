e2b3281930880b06320904cadbc42e9d7d6842cb
Daniel <dolboeb22441100@gmail.com>
2021-10-17 20:34:06 +0300

    summ

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 0D4F1DF8951B3287C4BB5CD137DAC49B7555
+
 const links = document.querySelectorAll("nav ul a");
  
 for (const link of links) {
@@ -17,3 +19,24 @@ let bm = document.querySelector(".burger-menu")
 bm.addEventListener("click", ()=>{
   bm.classList.toggle("burger-menu-active")
 })
+
+const pn = document.getElementById('pn').value
+const email = document.getElementById('email').value
+const formname = document.getElementById('name').value
+const message = document.getElementById('message').value
+
+function sendEmail() {
+  Email.send({
+    To : 'angelovdaniel24@gmail.com',
+    From : "Wedding order",
+    PhoneNumber : {pn},
+    Email : {email},
+    Name : {formname},
+    Message : {message}
+  })
+  .then(function (message) {
+  alert("Mail has been sent successfully")
+  });
+  }
+
+  console.log(pn)
