3964bc14d7d7def93470905f5ca7dea63e5c993f
Daniel <dolboeb22441100@gmail.com>
2021-10-18 14:41:07 +0300

    summ

--- main.js
+++ main.js
@@ -10,3 +10,27 @@ let bm = document.querySelector(".burger-menu")
 bm.addEventListener("click", ()=>{
   bm.classList.toggle("burger-menu-active")
 })
+
+
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    status.classList.add('success')
+    status.innerHTML = "Success";
+    form.reset()
+  }).catch(error => {
+    status.classList.add('error')
+    status.innerHTML = "Unsuccess"
+  });
+}
+form.addEventListener("submit", handleSubmit)
